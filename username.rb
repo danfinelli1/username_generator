c5775d8068d5203fad3f7b2eb9a90f172023a2c1
Dan Finelli <dan.finelli@hotmail.com>
2016-12-09 13:42:35 -0800

    everything except last one

--- username.rb
+++ username.rb
@@ -1,14 +1,27 @@
 # Make sure to run the tests in your /spec folder
 # Run `rspec /spec/username_spec.rb` to get started.
-
+$user_types = ["user", "seller", "manager", "admin"]
 def format_name(first, last)
-  nil
+  return nil if first.size==0 || last.size==0
+  first.gsub!(/\W|\d/, "")
+  last.insert(0, first.match(/^\w/).to_s).downcase.gsub(/\W|\d/, "")
 end
 
-def format_year
-  nil
+def format_year(year)
+  return nil if year.to_s.size!=4
+    year.to_s.match(/.{2}$/).to_s
 end
 
-def build_username
-  nil
+def check_privilege(index = 0)
+  index = 3 if index > 4
+  $user_types[index.floor]
+end
+
+def user_type_prefix(index=1)
+  return "" if index ==0 && index<1
+  check_privilege(index)+"-"
+end
+
+def build_username(first, last, year, index =0)
+  user_type_prefix(index)+format_name(first, last)+format_year(year)
 end
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,26 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase! + '!'
 end
 
-def first_char
-  "z"
+def first_char(word)
+  letter = word.match(/^\w/).to_s
+  letter.downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(word)
+  str = ""
+3.times do
+  str+=word.match(/#{word}/).to_s
+end
+return str
+end
+
+def after_you
+  return "no, after you"
 end
